eb47a9840240ee60c1823dd302ca9863ff146d24
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-11 16:42:45 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+  pigeon_hash = {}
+  pigeon_data.each do |key, value|
+    value.each do |descriptor, names|
+      names.each do |name|
+        if pigeon_hash[name]
+          pigeon_hash[name][key] << descriptor.to_s
+        else
+          pigeon_hash[name] = {:color => [], :gender => [], :lives => []}
+          pigeon_hash[name][key] << descriptor.to_s
+        end
+      end
+    end
+  end
+    pigeon_hash
 end
+
