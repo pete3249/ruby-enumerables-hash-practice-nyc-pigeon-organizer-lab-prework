ce0ef6719882255ab8fc9754521ed9dd92b115ac
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-11 16:41:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,19 @@
-
+def nyc_pigeon_organizer(pigeon_data)
+  pigeon_hash = {}
+  pigeon_data.each do |key, value|
+    value.each do |descriptor, names|
+      names.each do |name|
+        if pigeon_hash[name]
+          pigeon_hash[name][key] << descriptor.to_s
+        else
+          pigeon_hash[name] = {:color => [], :gender => [], :lives => []}
+          pigeon_hash[name][key] << descriptor.to_s
+        end
+      end
+    end
+  end
+    pigeon_hash
+end
 
 
 def nyc_pigeon_organizer(data)
